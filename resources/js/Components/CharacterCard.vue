91a280ad1a63d6b58268fbc4fdf5828a96b1e7c7
Sebastian Brear <ssbrear@ucdavis.edu>
2023-02-01 21:18:48 -0800

    Added links to armory, raiderio, and logs on the character card.

--- resources/js/Components/CharacterCard.vue
+++ resources/js/Components/CharacterCard.vue
@@ -16,11 +16,21 @@
             <div id="realm_name">{{ results.realm }}</div>
             <div id="guild">&lt; {{ results.guild }} &gt;</div>
         </div>
+        <div id="links">
+            <a target='_blank' :href="'https://worldofwarcraft.com/en-us/character/us/'+results.realm+'/'+results.name" id="armory">Armory</a>
+            <a target='_blank' :href="'https://www.warcraftlogs.com/character/us/'+results.realm.toLowerCase()+'/'+results.name" id="raiderio">Raider.io</a>
+            <a target='_blank' :href="'https://raider.io/characters/us/'+results.realm.toLowerCase()+'/'+results.name" id="warcraftlogs">Logs</a>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+
+        }
+    },
     props: {
         results: {
             name: String,
@@ -31,6 +41,6 @@ export default {
             headshot: String,
         },
         region: String,
-    },
+    }
 };
 </script>
