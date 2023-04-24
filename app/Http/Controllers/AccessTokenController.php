1bba423daa57da4257ee105123b1e19033d6c1e9
Sebastian Brear <ssbrear@ucdavis.edu>
2023-04-23 19:48:48 -0700

    testing prod

--- app/Http/Controllers/AccessTokenController.php
+++ app/Http/Controllers/AccessTokenController.php
@@ -9,7 +9,13 @@ class AccessTokenController extends Controller
 {
     public function getAccessToken(Request $request, $code) {
         $protocolHost = $request->getSchemeAndHttpHost();
-        if ($protocolHost !== "http://localhost:8000" && $protocolHost !== "https://intense-sierra-71683.herokuapp.com" && $protocolHost !== "https://www.wow-lp.com") return 'Invalid domain';
+        return $protocolHost;
+        if (
+            $protocolHost !== "http://localhost:8000"
+            && $protocolHost !== "https://intense-sierra-71683.herokuapp.com"
+            && $protocolHost !== "https://www.wow-lp.com"
+            && $protocolHost !== "https://wow-lp.herokuapp.com")
+                return 'Invalid domain';
         $postFields = [
             'redirect_uri' => $protocolHost.'/redirect',
             'grant_type' => 'authorization_code',
